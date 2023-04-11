40bfe84f2c9ac2ce76a8374cb946a9355cd0323e
codemikko <hellomikko@outlook.com>
2023-04-11 04:12:46 -0400

    See main.scss

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -31,155 +31,88 @@ export default function PostPage({
   let pageUrl = `${siteConfig.baseURL.replace(/\/$|$/, "/")}blog/${slug}`;
   return (
     <Layout metaTitle={title} metaDescription={description} ogImage={image}>
+
+
+
+
       <section className="section-sm pb-0">
         <div className="container">
-          <div className="row justify-content-center">
-            <div className="col-lg-10">
-              <div className="mb-5">
-                <h3 className="h1 mb-4 post-title">{title}</h3>
-
-                <ul className="card-meta list-inline mb-2">
-                  <li className="list-inline-item mt-2">
-                    <Link
-                      href={`/author/${author
-                        .replace(/ /g, "-")
-                        .toLowerCase()}`}
-                    >
-                      <a className="card-meta-author">
-                        {authors.map((authorPage, i) =>
-                          author.replace(/ /g, "-").toLowerCase() ===
-                          authorPage.authorSlug ? (
-                            <span key={i}>
-                              <Image
-                                src={authorPage.authorFrontMatter.image}
-                                alt={author}
-                                width="26"
-                                height="26"
-                                layout="fixed"
-                              />
-                            </span>
-                          ) : (
-                            ""
-                          )
-                        )}
-                        <i className="d-inline-block ms-2 ps-1 fst-normal">
-                          by <span>{author}</span>
-                        </i>
-                      </a>
-                    </Link>
-                  </li>
-                  <li className="list-inline-item mt-2">—</li>
-                  <li className="list-inline-item mt-2">
-                    <i className="me-2 tw-inline-block">
-                      <IconClock size={18} />
-                    </i>
-                    <span>{readingTime(content)} min read</span>
-                  </li>
-                  <li className="list-inline-item mt-2">—</li>
-                  <li className="list-inline-item mt-2">
-                    <i className="me-2 tw-inline-block">
-                      <IconCalendarEvent size={18} />
-                    </i>
-                    <span>{formatDate(date)}</span>
-                  </li>
-                </ul>
-              </div>
-            </div>
+          <div className="row justify-content">
             <div className="col-lg-12">
-              <div className="mb-5 text-center post-deatils-image">
+              <div className="mb-5 post-details-image">
                 <Image
                   className="rounded w-100"
                   src={image}
                   alt={title}
-                  width={`1120`}
-                  height={`595`}
+                  layout="fill"
+                  objectFit="cover"
+                  quality={100}
                   placeholder="blur"
                   blurDataURL={image}
+
                 />
+                <div className="post-hero-overlay">
+                  <div className="post-hero-content tw-pl-60">
+                    <h3 className="h1 mb-4 post-title text-white tw-absolute tw-bottom-36">{title}</h3>
+                    <ul className="post-meta-tag list-unstyled list-inline mt-5">
+                      <li className="list-inline-item">Tags: </li>
+                      {tags.map((t, i) => (
+                        <li key={i} className="list-inline-item">
+                          <Link href={`/tags/${t.replace(/ /g, "-").toLowerCase()}`}>
+                            <a className="bg-white text-dark">{t}</a>
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
+                    <ul className="card-meta list-inline mb-2">
+                      <li className="list-inline-item mt-2">
+                        <i className="me-2 tw-inline-block">
+                          <IconClock size={18} />
+                        </i>
+                        <span>{readingTime(content)} min read</span>
+                      </li>
+                      <li className="list-inline-item mt-2">—</li>
+                      <li className="list-inline-item mt-2">
+                        <i className="me-2 tw-inline-block">
+                          <IconCalendarEvent size={18} />
+                        </i>
+                        <span>{formatDate(date)}</span>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
               </div>
             </div>
-            <div className="col-lg-2 post-share-block order-1 order-lg-0 mt-5 mt-lg-0">
-              <div className="position-sticky" style={{ top: 150 + "px" }}>
-                <span className="d-inline-block mb-3 small">SHARE</span>
-                <ul className="social-share icon-box">
-                  <li className="d-inline-block d-lg-block me-2 mb-2">
-                    <a
-                      href={`https://twitter.com/intent/tweet?text=${title}&url=${pageUrl}`}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <i>
-                        <IconBrandTwitter size={18} />
-                      </i>
-                    </a>
-                  </li>
-                  <li className="d-inline-block d-lg-block me-2 mb-2">
-                    <a
-                      href={`https://www.facebook.com/sharer.php?u=${pageUrl}&quote=${title}`}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <i>
-                        <IconBrandFacebook size={18} />
-                      </i>
-                    </a>
-                  </li>
-                  <li className="d-inline-block d-lg-block me-2 mb-2">
-                    <a
-                      href={`https://www.linkedin.com/sharing/share-offsite/?url=${pageUrl}`}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <i>
-                        <IconBrandLinkedin size={18} />
-                      </i>
-                    </a>
-                  </li>
-                  <li className="d-inline-block d-lg-block me-2 mb-2">
-                    <a
-                      href={`https://www.reddit.com/submit?url=${pageUrl}`}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <i>
-                        <IconBrandReddit size={18} />
-                      </i>
-                    </a>
-                  </li>
-                  <li className="d-inline-block d-lg-block me-2 mb-2">
-                    <a
-                      href={`https://www.pinterest.com/pin/create/button/?&text=${title}&url=${pageUrl}&description=${title}`}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <i>
-                        <IconBrandPinterest size={18} />
-                      </i>
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <div className="col-lg-8 post-content-block order-0 order-lg-2">
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            <div className="col-lg-8 post-content-block order-0 order-lg-2 mx-auto">
               <div
-                className="content"
+                className="content text-justify"
                 dangerouslySetInnerHTML={{ __html: marked.parse(content) }}
               ></div>
-              <ul className="post-meta-tag list-unstyled list-inline mt-5">
-                <li className="list-inline-item">Tags: </li>
-                {tags.map((t, i) => (
-                  <li key={i} className="list-inline-item">
-                    <Link href={`/tags/${t.replace(/ /g, "-").toLowerCase()}`}>
-                      <a className="bg-white text-dark">{t}</a>
-                    </Link>
-                  </li>
-                ))}
-              </ul>
             </div>
           </div>
 
+
+          {/* Author section */}
           <div className="single-post-author">
-            <div className="row justify-content-center">
+            <div className="row justify-content">
               <div className="col-lg-10">
                 <div className="d-block d-md-flex">
                   <Link
@@ -188,7 +121,7 @@ export default function PostPage({
                     <a>
                       {authors.map((authorPage, i) =>
                         author.replace(/ /g, "-").toLowerCase() ===
-                        authorPage.authorSlug ? (
+                          authorPage.authorSlug ? (
                           <span key={i}>
                             <Image
                               src={authorPage.authorFrontMatter.image}
@@ -219,7 +152,7 @@ export default function PostPage({
                     </h3>
                     {authors.map((authorPage, i) =>
                       author.replace(/ /g, "-").toLowerCase() ===
-                      authorPage.authorSlug ? (
+                        authorPage.authorSlug ? (
                         <div
                           key={i}
                           dangerouslySetInnerHTML={{
@@ -252,10 +185,16 @@ export default function PostPage({
             </div>
           </div>
         </div>
-      </section>
+      </section >
+
+
+
+
+
+
 
       {useScripts("/js/lightense/lightense.min.js", "body", true)}
-    </Layout>
+    </Layout >
   );
 }
 
