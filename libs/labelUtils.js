90934fa8118e93ca9852793d62f760e88cc8e609
codemikko <hellomikko@outlook.com>
2023-04-15 09:06:42 -0400

    update updates.js

---
+++ libs/labelUtils.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+export async function getLabels(repo, owner) {
+    const url = `https://api.github.com/repos/${owner}/${repo}/labels`;
+
+    const config = {
+        headers: {
+            Authorization: `token ${process.env.NEXT_PUBLIC_GITHUB_TOKEN}`,
+        },
+    };
+
+    const { data } = await axios.get(url, config);
+    return data.map((label) => ({
+        name: label.name,
+        color: label.color,
+    }));
+}
+
+export async function getIssues(repo, owner) {
+    const url = `https://api.github.com/repos/${owner}/${repo}/issues`;
+
+    const config = {
+        headers: {
+            Authorization: `token ${process.env.NEXT_PUBLIC_GITHUB_TOKEN}`,
+        },
+    };
+
+    const { data } = await axios.get(url, config);
+    return data;
+}
--- pages/updates.js
+++ pages/updates.js
@@ -1,42 +1,75 @@
-// pages/updates.js
-import { getCommits, groupCommits } from "@/libs/commitUtils";
+import { getIssues } from "../libs/labelUtils";
 import Layout from "@/components/Layout";
-import axios from "axios"
+import Image from "next/image";
 
-export default function Updates({ commitGroups }) {
+export default function Updates({ issues }) {
     return (
         <Layout metaTitle={"Latest Updates"}>
-            <section className="section-sm pb-0">
-                <div className="container">
-                    {Object.entries(commitGroups).map(([category, commits]) => (
-                        <div key={category}>
-                            <h2>{category}</h2>
-                            <ul>
-                                {commits.map((commit) => (
-                                    <li key={commit.url}>
-                                        <a href={commit.url} target="_blank" rel="noopener noreferrer">
-                                            {commit.message}
-                                        </a>{" "}
-                                        - <span>{new Date(commit.date).toLocaleDateString()}</span>
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
-                    ))}
+            {/* <section
+                className="h-screen bg-cover bg-center"
+                style={{
+                    backgroundImage:
+                        "url(https://ik.imagekit.io/velora/Random_images/13170.jpg?updatedAt=1681557656596)",
+                }}
+            > */}
+            <section>
+                <div className="container mx-auto px-4">
+                    <h1 className="text-4xl font-bold text-white py-10">Updates</h1>
+                    <div className="grid grid-cols-2 gap-4">
+                        {issues &&
+                            issues.map((issue) => (
+                                <a
+                                    key={issue.id}
+                                    href={issue.html_url}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    <div className="card p-4 tw-shadow-lg tw-rounded-lg">
+                                        <div className="flex justify-between items-center">
+                                            <div className="text-lg font-bold">{issue.title}</div>
+                                            <div
+                                                className={`tw-text-sm tw-font-bold tw-uppercase tw-rounded-full tw-px-4 tw-py-1 tw-text-white ${getStatusLabel(
+                                                    issue.state
+                                                )}`}
+                                            >
+                                                {issue.state}
+                                            </div>
+                                        </div>
+                                        <p className="tw-mt-2">{issue.body}</p>
+                                    </div>
+                                </a>
+                            ))}
+                    </div>
+                    <div className="tw-fixed tw-top-0 tw-right-0">
+
+                        <Image
+                            className="tw-w-48 tw-h-48 tw-object-cover tw-opacity-50"
+                            src="https://ik.imagekit.io/velora/Assets/13170.jpg?updatedAt=1681562247555"
+                            alt="Globe"
+                            width={275}
+                            height={130}
+                        />
+                    </div>
                 </div>
             </section>
         </Layout>
     );
 }
 
-export async function getStaticProps() {
-    const commits = await getCommits("velora", "VeloraX");
-    const commitGroups = groupCommits(commits);
+export async function getServerSideProps() {
+    const issues = await getIssues("velora", "VeloraX");
+    return { props: { issues } };
+}
 
-    return {
-        props: {
-            commitGroups,
-        },
-        revalidate: 60 * 60, // Update every hour
-    };
+function getStatusLabel(status) {
+    switch (status.toLowerCase()) {
+        case "completed":
+            return "tw-bg-green-500";
+        case "in progress":
+            return "tw-bg-yellow-500";
+        case "pending":
+            return "tw-bg-gray-500";
+        default:
+            return "tw-bg-red-500";
+    }
 }
